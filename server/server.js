87fd409b5cecedc6780370dda276ce186ce194ff
Dung Hoang <75154204+trungdunghoang125@users.noreply.github.com>
2021-12-26 10:14:01 +0700

    config for deploy

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/web-ban-sach').then(() => {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://admin:1234@cluster0.ec2eh.mongodb.net/web-ban-sach?retryWrites=true&w=majority').then(() => {
     console.log('mongodb is connectinggg')
 });
 app.use('/api/uploads', uploadRouter);
@@ -42,6 +42,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 });
